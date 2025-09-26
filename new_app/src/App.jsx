d712ec40cb441cbf0e4a3bf1b7ecfea2ac49dd7b
sahi755 <sahilbhosale162@gmail.com>
2025-09-26 19:43:09 +0530

    get commit

--- new_app/src/App.jsx
+++ new_app/src/App.jsx
@@ -1,15 +1,14 @@
-
+import Hellow from "./compoent/Hello"
 
 function App() {
-
 
   return (
     <>
       <div>
-       
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+        <Hellow></Hellow>
       </p>
     </>
   )
---
+++ new_app/src/compoent/Hello.jsx
@@ -0,0 +1,8 @@
+import './compemscssof.css'
+function Hellow(){
+
+
+    return <h1 id="Hello">hello world</h1>
+}
+
+export default Hellow
